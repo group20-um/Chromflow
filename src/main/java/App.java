9a80995e1d88d5a184da2294f7d0c23f3bd31367
sweetcode <jan@krueger-jan.de>
2019-01-08 10:15:31 +0100

    new test stuff

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,22 @@
-import org.jblas.ComplexFloat;
-import org.jblas.ComplexFloatMatrix;
-import org.jblas.Eigen;
-import org.jblas.FloatMatrix;
+import cern.colt.matrix.DoubleMatrix2D;
+import cern.colt.matrix.ObjectFactory2D;
+import cern.colt.matrix.impl.DenseDoubleMatrix2D;
+import org.apache.commons.math3.linear.*;
+import org.ejml.simple.SimpleEVD;
+import org.jblas.*;
+import org.jblas.util.Logger;
+import org.jscience.JScience;
+import org.jscience.mathematics.vector.Float64Matrix;
+import org.jscience.mathematics.vector.Matrix;
+import org.ojalgo.array.Array1D;
+import org.ojalgo.matrix.PrimitiveMatrix;
+import org.ojalgo.matrix.decomposition.Eigenvalue;
+import org.ojalgo.scalar.ComplexNumber;
 
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Comparator;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
@@ -17,7 +27,7 @@ public class App {
         String fileName = args[0];
 
         System.out.println("########### READ FROM FILE ###########");
-        FloatMatrix m = null;
+        double[][] m = null;
         try {
             List<String> lines = Files.readAllLines(Paths.get(fileName));
 
@@ -26,7 +36,7 @@ public class App {
                 if(line.startsWith("VERTICES")) {
                     final int v = Integer.valueOf(line.split(" = ")[1]);
                     System.out.println(v);
-                    m = new FloatMatrix(v, v);
+                    m = new double[v][v];
                 } else if (!line.startsWith("EDGES") && !line.startsWith("//")) {
                     String[] split = line.split(" ");
 
@@ -38,7 +48,8 @@ public class App {
                     int from = Integer.parseInt(split[0]) - 1;
                     int to = Integer.parseInt(split[1]) - 1;
 
-                    m.put(from, to, 1F);
+                    m[from][to] = 1F;
+                    m[to][from] = 1F;
                 }
 
                 lineNumber++;
@@ -52,12 +63,41 @@ public class App {
         //--
 
         long time = System.currentTimeMillis();
-        ComplexFloatMatrix result = Eigen.eigenvalues(m);
-        float max = result.get(0).abs();
-        for(int i = 1; i < result.length; i++) {
-            max = Math.max(max, result.get(i).real());
+        //jblas
+        ComplexDoubleMatrix result = Eigen.eigenvalues(new DoubleMatrix(m));
+        for(ComplexDouble f : result.toArray()) {
+            System.out.println(f.abs());
         }
-        System.out.println(max);
+
+
+        /* apache - way too slow
+        RealMatrix t = MatrixUtils.createRealMatrix(m);
+        EigenDecomposition e = new EigenDecomposition(t);
+        System.out.println(Arrays.toString(e.getRealEigenvalues()));
+        */
+
+        //Float64Matrix t = Float64Matrix.valueOf(m);
+
+        //ojalgo
+        /*PrimitiveMatrix primitiveMatrix = PrimitiveMatrix.FACTORY.columns(m);
+        List<Eigenvalue.Eigenpair> result = primitiveMatrix.getEigenpairs();
+        result.forEach(e -> {
+            System.out.printf("%.2f\n", e.value.getReal());
+        });*/
+
+        //colt
+        /*DoubleMatrix2D matrix2D = new DenseDoubleMatrix2D(m);
+        cern.colt.matrix.linalg.EigenvalueDecomposition eigenDecomposition = new cern.colt.matrix.linalg.EigenvalueDecomposition(matrix2D);
+        System.out.println(Arrays.toString(eigenDecomposition.getRealEigenvalues().toArray()));*/
+
+        //ejml
+        /*org.ejml.data.DenseMatrix64F matrix = new org.ejml.data.DenseMatrix64F(m);
+        SimpleEVD eigen = new SimpleEVD(matrix);
+        for(int i = eigen.getIndexMin(); i <= eigen.getIndexMax(); i++) {
+            if(eigen.getEigenvalue(i).isReal())
+                System.out.println(eigen.getEigenvalue(i).getReal());
+        }*/
+
         System.out.println(System.currentTimeMillis() - time);
 
     }
